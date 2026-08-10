f4d9556794ae08d1632f1cffdf4f8c7d0b9e8c55
Saman Pandey <singularity.1@hotmail.com>
2026-08-10 22:56:04 +0530

    11Update webhook test message in LandingPage component

--- client/src/components/landing/LandingPage.jsx
+++ client/src/components/landing/LandingPage.jsx
@@ -85,7 +85,7 @@ const LandingPage = () => {
               Hello from <br />
               Dreamer. <br />
               <span className="font-bold text-gradient-gold">
-                your webhooks are working test10.
+                your webhooks are working test11.
               </span>
             </motion.h1>
 
