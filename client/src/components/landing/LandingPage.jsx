b0e9da00d948d68ea732da325746c1fd56bdf573
Saman Pandey <singularity.1@hotmail.com>
2026-08-10 22:09:23 +0530

    Update LandingPage.jsx

--- client/src/components/landing/LandingPage.jsx
+++ client/src/components/landing/LandingPage.jsx
@@ -86,7 +86,7 @@ const LandingPage = () => {
               Hello from <br />
               Dreamer. <br />
               <span className="font-bold text-gradient-gold">
-                your webhooks are working.
+                your webhooks are working test3.
               </span>
             </motion.h1>
 
